2e680bce5a4c7562b87d958c3f8756fa3fee8190
tonym <tonymaltas@yahoo.co.uk>
2020-10-10 18:36:15 +0100

    add material toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
+import {MatCardModule, MatInputModule, MatButtonModule, MatToolbarModule, MatIconModule} from '@angular/material';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
